5edcb211df7ddca597ec8b50b4e1b91a9a9f9683
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2021-12-08 23:07:08 +0100

    começo

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { application, response } from 'express';
 
 const app = express();
-
+app.get('/', (request, response) => {
+  return response.json({mesager: 'Hello word!'})
+});
 app.listen(5555);
